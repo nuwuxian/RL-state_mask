d5d8d0d75a6b70cc79a13b1a7d7756ad6161b6a9
Zelei Cheng <38622719+chengzelei@users.noreply.github.com>
2022-10-16 17:29:26 -0400

    update

--- extensive_form/imperfect_games/DouZero/Retrain/evaluate.py
+++ extensive_form/imperfect_games/DouZero/Retrain/evaluate.py
@@ -7,14 +7,20 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
                     'Dou Dizhu Evaluation')
     parser.add_argument('--landlord', type=str,
-            default='baselines/douzero_ADP/landlord.ckpt')
+            default='/data/zelei/DouZero_lasso_0.06/baselines/douzero_WP/landlord.ckpt')
     parser.add_argument('--landlord_up', type=str,
-            default='baselines/sl/landlord_up.ckpt')
+            default='/data/zelei/DouZero_lasso_0.06/baselines/douzero_WP/landlord_up.ckpt')
     parser.add_argument('--landlord_down', type=str,
-            default='baselines/sl/landlord_down.ckpt')
+            default='/data/zelei/DouZero_lasso_0.06/baselines/douzero_WP/landlord_down.ckpt')
+    parser.add_argument('--retrain_landlord', type=str,
+            default='/data/zelei/DouZero_lasso_0.06/DouZero-main/retrain_checkpoints/douzero/landlord_weights_83200.ckpt')
+    parser.add_argument('--landlord_up', type=str,
+            default='/data/zelei/DouZero_lasso_0.06/baselines/douzero_WP/landlord_up.ckpt')
+    parser.add_argument('--landlord_down', type=str,
+            default='/data/zelei/DouZero_lasso_0.06/baselines/douzero_WP/landlord_down.ckpt')
     parser.add_argument('--eval_data', type=str,
             default='eval_data.pkl')
-    parser.add_argument('--num_workers', type=int, default=5)
+    parser.add_argument('--num_workers', type=int, default=10)
     parser.add_argument('--gpu_device', type=str, default='')
     args = parser.parse_args()
 
@@ -22,6 +28,12 @@ if __name__ == '__main__':
     os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_device
 
     evaluate(args.landlord,
+             args.landlord_up,
+             args.landlord_down,
+             args.eval_data,
+             args.num_workers)
+    
+    evaluate(args.retrain_landlord,
              args.landlord_up,
              args.landlord_down,
              args.eval_data,
