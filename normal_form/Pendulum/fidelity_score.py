de201c9593218894129ccf71a2a635c08ed1bec1
Zelei Cheng <38622719+chengzelei@users.noreply.github.com>
2023-10-28 18:33:29 -0500

    Update fidelity_score.py

--- normal_form/Pendulum/fidelity_score.py
+++ normal_form/Pendulum/fidelity_score.py
@@ -5,17 +5,13 @@ critical_steps_ends = np.loadtxt("./recording/critical_steps_ends.out")
 
 replay_rewards = np.loadtxt("./recording/replay_reward_record.out")
 original_rewards = np.loadtxt("./recording/reward_record.out")
-
 print(np.mean(original_rewards))
-
 
 p_ls = []
 p_ds = []
-
 fidelity_scores = []
 
 for i_episode in range(500):
-    
     iteration_ends_path = "./recording/eps_len_" + str(i_episode) + ".out"
     iteration_ends = np.loadtxt(iteration_ends_path)
 
@@ -24,14 +20,9 @@ for i_episode in range(500):
     random_replacement_steps = critical_frames_end - critical_frames_start
 
     p_ls.append(random_replacement_steps/iteration_ends)
-
     p_ds.append(abs(replay_rewards[i_episode] - original_rewards[i_episode]))
-
-    fidelity_scores.append(np.log(p_ls[-1]) - np.log(p_ds[-1] + 1e-10))
 
 p_l = np.mean(p_ls)
 p_d = np.mean(p_ds/np.max(p_ds))
 
-print(p_l)
 print(np.log(p_l) - np.log(p_d))
-print(np.mean(fidelity_scores))
